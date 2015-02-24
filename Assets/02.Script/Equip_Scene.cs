017c88c4d3791727485f316efe2f1977adc25b9a
sue0711 <godqlswlq@gmail.com>
2015-02-24 14:47:59 +0900

    되랏 이얍

---
+++ Assets/02.Script/Equip_Scene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equip_Scene : MonoBehaviour {
+
+	public string selectedEp = "Monday";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Fadeout () {
+		float fadeTime = GameObject.Find ("Fading").GetComponent<Fading>().BeginFade(1);
+		Invoke ("GoToGame", fadeTime);
+	}
+
+	public void StartClicked(){
+		Debug.Log ("Start Button Clicked");
+		Fadeout ();
+	}
+
+	void GoToGame(){
+		selectedEp.Substring(0,selectedEp.Length-1);
+		Application.LoadLevel (selectedEp);
+	}
+}
--- Assets/02.Script/Select_Scene.cs
+++ Assets/02.Script/Select_Scene.cs
@@ -1,41 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonControl : MonoBehaviour {
+public class Select_Scene : MonoBehaviour {
 	
-	string selectedEp;
-
+	//public string selectedEp;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void Fadeout () {
 		float fadeTime = GameObject.Find ("Fading").GetComponent<Fading>().BeginFade(1);
-		selectedEp.Substring(0,selectedEp.Length-1);
-		Invoke (selectedEp, fadeTime);
+		//selectedEp.Substring(0,selectedEp.Length-1);
+		Invoke ("GoToEquip", fadeTime);
 	}
 
+	//top_menu
 	public void ClickedCollection(){
 		Debug.Log ("Collection Button Clicked");
 	}
-
+	
 	public void ClickedSetting(){
 		Debug.Log ("Setting Button Clicked");
 	}
 
+	//episode_select
 	public void ClickedMonday(){
 		Debug.Log ("Monday Selected");
-		selectedEp = "GoToMonday";
+		//selectedEp = "Monday";
 		Fadeout ();
 	}
 
-	public void GoToMonday(){
-		Application.LoadLevel ("scene1");
+
+	void GoToEquip(){
+		Application.LoadLevel ("Equip_Scene");
 	}
 }
