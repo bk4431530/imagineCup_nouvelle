7d3c90fbd2bd7c605f3ec9dd813aeef9b260c0e9
sue0711 <godqlswlq@gmail.com>
2015-03-05 17:21:44 +0900

    오디오부분 전면수정
    ㅇㅇ

--- Assets/02.Script/Option.cs
+++ Assets/02.Script/Option.cs
@@ -4,36 +4,67 @@ using UnityEngine.UI;
 
 
 public class Option : MonoBehaviour {
-
-
-	public Toggle MusicOn;
 	
-	public AudioSource backMusic;
+	
+	Toggle MusicOn;
+	Toggle MusicOff;
+	
+	Toggle VibOn;
+	Toggle VibOff;
+	
+	AudioSource backMusic;
 
+	void Awake(){
+		MusicOn = GameObject.Find ("MusicOn").GetComponent<Toggle> ();
+		MusicOff = GameObject.Find ("MusicOff").GetComponent<Toggle> ();
+		
+		VibOn = GameObject.Find ("VibOn").GetComponent<Toggle> ();
+		VibOff = GameObject.Find ("VibOff").GetComponent<Toggle> ();
+		
+		backMusic = GameObject.Find ("Audio Source").GetComponent<AudioSource> ();
 
+	}
+	
 	void Start () {
 
-		
 		if(GameManager.bgm == true)
 		{
-			MusicOn.isOn = true;//toggle on
+			//MusicOn button On
+			MusicOn.isOn = true;
+			MusicOff.isOn = false;
 			backMusic.Play();
 			
 		}
 		else
 		{
-			MusicOn.isOn = false;//toggle off
+			//MusicOn button off
+			MusicOn.isOn = false;
+			MusicOff.isOn = true;
 			backMusic.Stop();
 		}
-
-	
+		
+		if(GameManager.vibration == true)
+		{
+			// VibOn button on
+			VibOn.isOn = true;
+			VibOff.isOn = false; 
+			
+		}
+		else
+		{
+			// VibOn button off
+			VibOn.isOn = false;
+			VibOff.isOn = true;
+		}
+		
+		
 	}
 	
 	void Update () {
-	
+		
 	}
-
-
+	
+	
 	
 	public void music_On()
 	{
@@ -44,7 +75,7 @@ public class Option : MonoBehaviour {
 			
 		}
 	}
-
+	
 	public void music_Off()
 	{
 		if(GameManager.bgm == true)
@@ -55,19 +86,26 @@ public class Option : MonoBehaviour {
 		}
 	}
 	
+	////////////////////////////////////   
 	
-	/*
-	public void vib_OnOff()
+	
+	public void vib_On()
+	{
+		if(GameManager.vibration == false)
+		{
+			GameManager.vibration = true; //true로 바꿈
+			
+		}
+	}
+	
+	public void vib_Off()
 	{
 		if(GameManager.vibration == true)
 		{
 			GameManager.vibration = false; //false로 바꿈
 			
-		}else
-		{
-			GameManager.vibration = true; //true 로 바꿈 
 		}
 	}
-*/
-
+	
+	
 }
