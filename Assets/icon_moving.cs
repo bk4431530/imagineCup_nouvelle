cb689bb02106e2f434b52baa2f0ec4b9fc5e862e
sue0711 <godqlswlq@gmail.com>
2015-03-06 22:22:56 +0900

    aaa
    aaa

---
+++ Assets/icon_moving.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class icon_moving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("icon_path"), "orienttopath", true, "time", 50));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
