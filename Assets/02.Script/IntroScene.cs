6e3b191510958686e4661711fc67f2942f9e2311
sue0711 <godqlswlq@gmail.com>
2015-03-10 02:13:24 +0900

    intro input
    ohohoh

--- Assets/02.Script/IntroScene.cs
+++ Assets/02.Script/IntroScene.cs
@@ -9,15 +9,20 @@ public class IntroScene : MonoBehaviour {
 	public GameObject letter;
 	public GameObject letterGuide;
 
+	public GameObject textview;
+	public GameObject onLetter;
 
 	private Vector2 ray;
 	private RaycastHit2D hit;
 
-	Text letterBox;
+	Text letterText;
+	Text onletterText;
 
 	string letterInput;
+	string context;
 
 	bool letterbox_ison = false;
+	bool input_isfocused = false;
 
 	//bgm_setting
 	
@@ -26,19 +31,25 @@ public class IntroScene : MonoBehaviour {
 	// Use this for initialization
 	void OnEnable ()
 	{
-		letterBox = GameObject.Find ("/Canvas/Input/Text").GetComponent<Text> ();
+		letterGuide = GameObject.Find ("LetterGuide");
+		textview = GameObject.Find("View");
+		letter = GameObject.Find("TextInput");
+		onLetter = GameObject.Find ("LetterText");
+
+		letterText = GameObject.Find ("TextView").GetComponent<Text> ();
+		onletterText = GameObject.Find ("LetterText").GetComponent<Text> ();
+
+		textview.SetActive (false);
+		letterGuide.SetActive (false);
+		onLetter.SetActive (false);
+
 		guide = GameObject.Find("/Canvas/Panel/Guide");
 		guide.SetActive (false);
 
-		letter = GameObject.Find ("/Canvas/Input");
-		letter.SetActive (false);
 
-		letterGuide = GameObject.Find ("Canvas/LetterGuide");
-		letterGuide.SetActive (false);
-
-		backMusic = GameObject.Find ("BGM").GetComponent<AudioSource> ();
+		//backMusic = GameObject.Find ("BGM").GetComponent<AudioSource> ();
 		if (GameManager.bgm) {
-			backMusic.Play();		
+			//backMusic.Play();		
 		}
 	//	letterBox = GameObject.Find ("letterInput").GetComponent<Text> ();
 	//	Debug.Log("letterTxt is (" + letterInput + " )");
@@ -49,7 +60,8 @@ public class IntroScene : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		letterInput = letterBox.text;
+		letterInput = letterText.text;
+		onletterText.text =letterText.text;
 
 		Debug.Log("Script: IntroScene.cs // _currentItemIndex = " + LevelMenu2D._currentItemIndex );
 
@@ -76,9 +88,19 @@ public class IntroScene : MonoBehaviour {
 			}
 		}
 
+		if (letter.GetComponent<InputField> ().isFocused) {
+			if(input_isfocused == false)
+			{
+				input_isfocused = true;
+			}
+		}
 
-
-
+		if (input_isfocused == true) {
+			if (letter.GetComponent<InputField> ().isFocused == false){
+				textview.SetActive(false);
+				input_isfocused = false;
+			}
+		}
 
 
 		//Scene8_ guide set activve
@@ -95,17 +117,18 @@ public class IntroScene : MonoBehaviour {
 				Debug.Log("Level Count Reset : " + LevelMenu2D._currentItemIndex );
 				FadeOut();
 			}*/
-		} 
+		} 	
+	}
 
-
-	
+	public void clickedWrite(){
+		textview.SetActive (true);
 	}
 
 	public void GoToSelectScene()
 	{
 		LevelMenu2D._currentItemIndex = 0;
 		Debug.Log("Go to Select Scene");
-
+		//backMusic.GetComponent<AudioSource>().clip = (AudioClip) Resources.Load (null);
 		Application.LoadLevel ("Select_Scene");
 
 	}
@@ -113,16 +136,15 @@ public class IntroScene : MonoBehaviour {
 	public void FadeOut ()
 	{
 		float fadeTime = GameObject.Find ("Fading").GetComponent<Fading>().BeginFade(1);
-		backMusic.GetComponent<AudioSource>().clip = (AudioClip) Resources.Load (null);
+
 		Invoke ("GoToSelectScene", fadeTime);
 
 	}
 
 	public void EnableLetterBox()
 	{
-		letter.SetActive(true);
-		letterGuide.SetActive(true);
-
+		letterGuide.SetActive (true);
+		onLetter.SetActive (true);
 		//Debug.Log("letterTxt is (" + letterInput + " )");
 
 	}
@@ -130,8 +152,9 @@ public class IntroScene : MonoBehaviour {
 
 	public void DisableLetterBox()
 	{
-		letter.SetActive(false);
-		letterGuide.SetActive(false);
+		textview.SetActive (false);
+		letterGuide.SetActive (false);
+		onLetter.SetActive (false);
 	}
 
 
