6e16503bacc52fe5ce73e14894461a2b829be4f0
sue0711 <godqlswlq@gmail.com>
2015-03-23 11:46:39 +0900

    다시다시

--- Assets/02.Script/Equip_Scene.cs
+++ Assets/02.Script/Equip_Scene.cs
@@ -2,222 +2,356 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Tutorial_Monday : MonoBehaviour {
+public class Equip_Scene : MonoBehaviour {
 	
-	public GameObject Tut_Canvas;
-	public GameObject Tut_bg;
-	public GameObject Tut_help1;
-	public GameObject Tut_help2;
-	public GameObject Tut_help3;
-	public GameObject Tut_help4;	
+	GameObject booster;
+	GameObject magnet;
+	GameObject shield;
+	GameObject mysteryBox;
 	
-	private int help_num = 0;
-	private bool help2 = false;
-	private bool help3 = false;
-	private bool help4 = false;
+	int item_cnt = 0;
 	
-	public GameObject player;
-	public GameObject circle;
-	public GameObject puzzle;
-	public GameObject cat;
+	GameObject buyItem_popup;
+	GameObject loading_screen;
 	
-	public GameObject loadingScreen;
-	AudioSource backmusic;
+	int items;
+	Text itemTxt;
+	public GameObject randomItem;
 	
-	// Use this for initialization
-	void Start ()
-	{
-		player = GameObject.Find ("player");
-		Tut_Canvas = GameObject.Find ("TutorialCanvas");
-		//Tut_bg = GameObject.Find ("/TutorialCanvas/Background");
-		Tut_help1 = GameObject.Find ("/TutorialCanvas/Help1");
-		Tut_help2 = GameObject.Find ("/TutorialCanvas/Help2");
-		Tut_help3 = GameObject.Find ("/TutorialCanvas/Help3");
-		Tut_help4 = GameObject.Find ("/TutorialCanvas/Help4");
+	Text booster_cnt;
+	Text magnet_cnt;
+	Text shield_cnt;
+	Text mysteryBox_cnt;
+	
+	AudioSource backMusic;
+	
+	
+	string selectedScene;
+	//GameObject thumbnail;
+	public GameObject[] effect;
+	//public Sprite[] thumbnail_Img;
+	public Sprite[] effect_Img;
+	public Sprite[] item_Img;
+	
+	
+	//bgm
+	string audio_name;
+	
+	//sound effect
+	public static AudioSource SFX_button;
+	public static AudioSource SFX_equipitem;
+	public static AudioSource SFX_itemBuy;
+	
+	public int money;
+	
+	void Start () {
+		booster = GameObject.Find ("Booster");
+		magnet = GameObject.Find ("Magnet");
+		shield = GameObject.Find ("Shield");
+		mysteryBox = GameObject.Find ("MysteryBox");
+		buyItem_popup = GameObject.Find ("Popup_buyItem");
+		loading_screen = GameObject.Find ("Loading_Screen");
+		//thumbnail = GameObject.Find ("Canvas/leftMenu/Image");
+		effect[0] = GameObject.Find ("Canvas/leftMenu/effectsBar/mask1/effect1");
+		effect[1] = GameObject.Find ("Canvas/leftMenu/effectsBar/mask2/effect2");
+		effect[2] = GameObject.Find ("Canvas/leftMenu/effectsBar/mask3/effect3");
 		
-		circle = GameObject.Find ("circle");
-		puzzle = GameObject.Find ("puzzle");
 		
-		loadingScreen = GameObject.Find ("Loading_Screen");
+		backMusic = GameManager.backMusic;
+		itemTxt = GameObject.Find ("ItemTxt").GetComponent<Text> ();
+
+		booster_cnt = booster.transform.FindChild("count").GetComponent<Text> ();
+		magnet_cnt = magnet.transform.FindChild("count").GetComponent<Text> ();
+		shield_cnt = shield.transform.FindChild("count").GetComponent<Text> ();
+		mysteryBox_cnt = mysteryBox.transform.FindChild("count").GetComponent<Text> ();
+
+		buyItem_popup.SetActive (false);
+		loading_screen.SetActive (false);
+
+		GameManager.quillPen = 1000;
+		GameManager.currentEpisode = 1;
 		
-		backmusic = GameManager.backMusic;
-		if (GameManager.bgm) {
-			backmusic.Play ();		
+		//sound effect
+		SFX_button = GameObject.Find ("/SFX/button").GetComponent<AudioSource>();	
+		SFX_button.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("ButtonClick");
+		
+		SFX_equipitem = GameObject.Find ("/SFX/equipItem").GetComponent<AudioSource> ();
+		SFX_equipitem.GetComponent<AudioSource> ().clip = (AudioClip)Resources.Load ("Equip");
+		
+		SFX_itemBuy = GameObject.Find ("/SFX/itemBuy").GetComponent<AudioSource> ();
+		SFX_itemBuy.GetComponent<AudioSource> ().clip = (AudioClip)Resources.Load ("ItemBuy");
+		
+		//수정중
+		/*	
+		booster_cnt.text = GameManager.booster.ToString();
+		Debug.Log ("booster_cnt.text = " + booster_cnt.text);
+
+		magnet_cnt.text = GameManager.magnet.ToString();
+		Debug.Log ("magnet_cnt.text = " + magnet_cnt.text);
+
+		shield_cnt.text = GameManager.shield.ToString();
+		Debug.Log ("shield_cnt.text = " + shield_cnt.text);
+
+		mysteryBox_cnt.text = GameManager.mysteryBox.ToString();
+		Debug.Log ("mysteryBox_cnt.text = " + mysteryBox_cnt.text); */
+		
+		switch (GameManager.currentEpisode) {
+		case 1:
+			if(GameManager.Tutorial_Monday)
+			{
+				Debug.Log("monday_tutorial");
+				selectedScene = "Monday_Tutorial";
+			} else {
+				Debug.Log("monday");
+				selectedScene = "Monday";
+				//thumbnail.GetComponent<Image>().sprite = thumbnail_Img[0];
+			}
+			audio_name="BGM_Monday";
+			break;
+		case 2:
+			selectedScene = "Tuesday";
+			//thumbnail.GetComponent<Image>().sprite = thumbnail_Img[1];
+			break;
+		case 3:
+			selectedScene = "Wednesday";
+			//thumbnail.GetComponent<Image>().sprite = thumbnail_Img[2];
+			break;
+		case 4:
+			selectedScene = "Thursday";
+			//thumbnail.GetComponent<Image>().sprite = thumbnail_Img[3];
+			break;
+		case 5:
+			selectedScene = "Friday";
+			//thumbnail.GetComponent<Image>().sprite = thumbnail_Img[4];
+			break;
+		default:
+			Debug.Log("selected Episode = null");
+			break;
 		}
-		DisableTutorialCanvas ();
-		
-		circle.SetActive (false);
-		loadingScreen.SetActive (false);
-		
-		StartTutorial();
-		EnableHelp1 ();
-		
-		Invoke ("Timezero", 1.0f);
-		PlayerControl_tutorial.PSpause = true;
 	}
+
+	void Update(){
+				booster_cnt.text = GameManager.booster.ToString ();
+				magnet_cnt.text = GameManager.magnet.ToString ();
+				shield_cnt.text = GameManager.shield.ToString ();
+				mysteryBox_cnt.text = GameManager.mysteryBox.ToString (); 
+		
+		
+				for (int i =0; i<3; i++) {
+						if (GameManager.paperPlaneState [i] != 0) {
+								switch (GameManager.paperPlaneState [i]) {
+								case 0: 
+										effect [i].GetComponent<Image> ().sprite = effect_Img [0];
+										break;
+								case 1:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [1];
+										break;
+								case 2:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [2];
+										break;
+								case 3:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [3];
+										break;
+								case 4:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [4];
+										break;
+								case 5:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [5];
+										break;
+								case 6:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [6];
+										break;
+								case 7:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [7];
+										break;
+								case 8:
+										effect [i].GetComponent<Image> ().sprite = effect_Img [8];
+										break;
+								}//switch
+						}//if
+				}//for
+		}
+		void LoadingScreen () {
+			loading_screen.SetActive (true);
+			Invoke ("GoToGame", 5.0f);
+		}
+		public void StartClicked(){
+			Debug.Log ("Start Button Clicked");
+			LoadingScreen ();
+		}
+
+	void GoToGame(){
+				backMusic.GetComponent<AudioSource> ().clip = (AudioClip)Resources.Load (audio_name);
+				loading_screen.SetActive (true);
+				Application.LoadLevel (selectedScene);
+		}
+		//items
+		
+	public void ClickedBoosterEquip(){
+			if (GameManager.booster_equip == false && item_cnt < 3 && GameManager.booster >0) {
+				booster.GetComponent<Image> ().color = Color.Lerp (Color.black,Color.white,0.9f);
+				GameManager.booster_equip = true;
+				item_cnt++;
+				Debug.Log ("Booster equiped" + "  &  item_cnt = " + item_cnt);
+			} else if (GameManager.booster_equip == true){
+				booster.GetComponent<Image> ().color = Color.Lerp (Color.white,Color.white,1.0f);
+				GameManager.booster_equip = false;
+				item_cnt--;
+				Debug.Log ("Booster unequiped" + "  &  item_cnt = " + item_cnt);
+			}
+			
+		}
+
+	public void ClickedMagnetEquip(){
+		if (GameManager.magnet_equip == false && item_cnt < 3 && GameManager.magnet >0) {
+			magnet.GetComponent<Image> ().color = Color.Lerp (Color.black,Color.white,0.9f);
+			GameManager.magnet_equip = true;
+			item_cnt++;
+			Debug.Log ("Magnet equiped" + "  &  item_cnt = " + item_cnt);
+		} else if (GameManager.magnet_equip == true){
+			magnet.GetComponent<Image> ().color = Color.Lerp (Color.white,Color.white,1.0f);
+			GameManager.magnet_equip = false;
+			item_cnt--;
+			Debug.Log ("Magnet unequiped" + "  &  item_cnt = " + item_cnt);
+		}
+	}
+
+	public void ClickedShieldEquip(){
+		if (GameManager.shield_equip == false && item_cnt < 3 && GameManager.shield >0) {
+			shield.GetComponent<Image> ().color = Color.Lerp (Color.black,Color.white,0.9f);
+			GameManager.shield_equip = true;
+			item_cnt++;
+			Debug.Log ("Shield equiped" + "  &  item_cnt = " + item_cnt);
+		} else if (GameManager.booster_equip == true){
+			shield.GetComponent<Image> ().color = Color.Lerp (Color.white,Color.white,1.0f);
+			GameManager.shield_equip = false;
+			item_cnt--;
+			Debug.Log ("Shield unequiped" + "  &  item_cnt = " + item_cnt);
+		}
+	}
+
+	public void ClickedMysteryBoxEquip(){
+		if (GameManager.mysteryBox_equip == 0 && item_cnt < 3 && GameManager.mysteryBox >0) {
+			mysteryBox.GetComponent<Image> ().color = Color.Lerp (Color.black,Color.white,0.9f);
+			GameManager.mysteryBox_equip = 1;
+			item_cnt++;
+			Debug.Log ("MysteryBox equiped" + "  &  item_cnt = " + item_cnt);
+		} else if (GameManager.mysteryBox_equip > 0){
+			mysteryBox.GetComponent<Image> ().color = Color.Lerp (Color.white,Color.white,1.0f);
+			GameManager.mysteryBox_equip = 0;
+			item_cnt--;
+			Debug.Log ("MysteryBox unequiped" + "  &  item_cnt = " + item_cnt);
+		}
+	}
+
 	
-	// Update is called once per frame
-	void Update () {
-		if(TouchHandler.Mswiped == true && help_num == 1)
+	//item popup
+	public void clickedBuyItems(int i)
+	{//buyItem_popup.SetActive (true);
+		items = i;
+		bool lack = false;
+		
+		switch (items) 
 		{
-			DisableHelp1();
-			PlayerControl_tutorial.PSpause = false;
+		case 0:
+			if(GameManager.quillPen > GameManager.booster_price)
+			{
+				GameManager.quillPen = GameManager.quillPen - GameManager.booster_price;
+				GameManager.booster++;
+			}else{
+			}
+			break;
+		case 1:
+			if(GameManager.quillPen > GameManager.magnet_price)
+			{
+				GameManager.quillPen = GameManager.quillPen - GameManager.magnet_price;
+				GameManager.magnet++;
+				
+			}else{
+			}
+			break;
+		case 2:
+			if(GameManager.quillPen > GameManager.shield_price)
+			{
+				GameManager.quillPen = GameManager.quillPen - GameManager.shield_price;
+				GameManager.shield++;
+				
+			}else{
+			}
+			break;
+		case 3:
+			if(GameManager.quillPen > GameManager.mysteryBox_price)
+			{
+				buyItem_popup.SetActive (true);
+				GameManager.quillPen = GameManager.quillPen - GameManager.mysteryBox_price;
+				mysterybox();
+				
+			}else{
+				itemTxt.text ="You don't have enough Quilpens!!";
+			}
+			break;
 		}
-		
-		if (player.transform.position.x > 39 && player.transform.position.x < 40 && help_num == 0 && !help2) {
-			EnableHelp2();
-			PlayerControl_tutorial.PSpause = true;
-			help2 = true;
+	}
+
+	void mysterybox()
+	{
+		string RandomItem ="";
+		int rand = (int)Random.Range(0,3);
+		switch (rand) {		
+		case 0:
+			RandomItem ="booster!";
+			randomItem.GetComponent<Image>().sprite = item_Img[0];
+			GameManager.booster++;	
+			break;
+		case 1:
+			RandomItem ="magnet!";
+			randomItem.GetComponent<Image>().sprite = item_Img[1];
+			GameManager.magnet++;
+			break;
+		case 2:
+			RandomItem ="shield!";
+			randomItem.GetComponent<Image>().sprite = item_Img[2];
+			GameManager.shield++;
+			break;
 		}
-		
-		if (player.GetComponent<CatchedbyCat_tutorial> ().PS_cat == PlayerState_cat.CatchedByCat && help_num == 0 && !help3) {
-			EnableHelp3();
-			PlayerControl_tutorial.PSpause = true;
-			help3 = true;
+		itemTxt.text ="You get " + RandomItem;
+		Invoke("clickClose",0.8f);
+	}
+
+	public void clickClose()
+	{
+		buyItem_popup.SetActive (false);
+	}
+
+	public void ButtonSound()
+	{
+		if(GameManager.sfx)
+		{
+			//SFX_button.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("ButtonClick");
+			SFX_button.Play();
+			Debug.Log("Equip.cs에서 ButtonSound 함수실행");
+			
 		}
-		
-		if (player.GetComponent<CatchedbyCat_tutorial> ().PS_cat == PlayerState_cat.Free && help_num == 3 && help3) {
-			DisableHelp3 ();
+	}
+
+	public void EquipItemSound()
+	{
+		if(GameManager.sfx)
+		{
+			SFX_equipitem.Play ();
+			Debug.Log("Equip.cs에서 EquipItemSound() 실행");
+			
 		}
-		
-		if (PlayerControl_tutorial.isClear && !help4) {
-			EnableHelp4();
-			help4 = true;
+	}
+
+	public void BuyItemSound()
+	{
+		if(GameManager.sfx)
+		{
+			SFX_itemBuy.Play ();
+			Debug.Log("Equip.cs에서 BuyItemSound() 실행");
+			
 		}
 	}
-	
-	/////////
-	void Timezero(){
-		Time.timeScale = 0;
-	}
-	
-	/////////////////////////////////
-	
-	public void DisableTutorialCanvas()
-	{
-		
-		
-		Tut_help1.SetActive (false);
-		Tut_help2.SetActive (false);
-		Tut_help3.SetActive (false);
-		Tut_help4.SetActive (false);
-		
-		//Tut_bg.SetActive (false);
-		Tut_Canvas.SetActive (false);
-		
-		//playTutorial = false;
-		
-		Debug.Log ("DisableTutorialCanvas() 실행");
-		
-	}
-	
-	////////////////////////////////////////////////////
-	
-	public void StartTutorial()
-	{
-		Tut_Canvas.SetActive (true);
-		//Tut_bg.SetActive (true);
-		Debug.Log ("StartTutorial() 실행");
-	}
-	
-	
-	///////////////////////////////////////////////////   
-	
-	/// help1
-	public void EnableHelp1()
-	{
-		Tut_help1.SetActive (true);
-		Debug.Log ("EnableHelp1() 실행");
-		help_num = 1;
-	}
-	
-	public void DisableHelp1()
-	{
-		Time.timeScale = 1;	
-		Tut_help1.SetActive (false);
-		Debug.Log ("DisableHelp1() 실행");
-		help_num = 0;
-	}
-	
-	// help2
-	public void EnableHelp2()
-	{
-		Timezero ();
-		circle.SetActive (true);
-		Tut_help2.SetActive (true);
-		Debug.Log ("EnableHelp2() 실행");
-		help_num = 2;
-	}
-	
-	public void DisableHelp2()
-	{
-		circle.SetActive (false);
-		Tut_help2.SetActive (false);
-		Debug.Log ("DisableHelp2() 실행");
-		help_num = 0;
-		Time.timeScale = 1;
-		PlayerControl_tutorial.PSpause = false;
-	}
-	
-	//help3
-	public void EnableHelp3()
-	{
-		Timezero ();
-		Tut_help3.SetActive (true);
-		Debug.Log ("EnableHelp3() 실행");
-		help_num = 3;
-	}
-	
-	public void DisableHelp3()
-	{
-		Tut_help3.SetActive (false);
-		Debug.Log ("DisableHelp3() 실행");
-		help_num = 0;
-		Time.timeScale = 1;
-		PlayerControl_tutorial.PSpause = false;
-	}
-	
-	//help4
-	public void EnableHelp4()
-	{
-		Tut_help4.SetActive (true);
-		Debug.Log ("EnableHelp4() 실행");
-		help_num = 4;
-	}
-	
-	public void DisableHelp4()
-	{
-		Debug.Log ("DisableHelp4() 실행");
-		help_num = 0;
-		loadingScreen.SetActive (true);
-		//Invoke ("GoToGame", 2.0f);
-		
-		GameManager.Tutorial_Monday = false;
-		PlayerPrefsX.SetBool ("TutorialMonday", GameManager.Tutorial_Monday);
-		
-		Application.LoadLevel ("Monday");
-	}
-	
-	/////SkipButton  
-	public void SkipButtonClicked()
-	{
-		loadingScreen.SetActive (true);
-		//Invoke ("GoToGame", 4.0f);
-		
-		GameManager.Tutorial_Monday = false;
-		PlayerPrefsX.SetBool ("TutorialMonday", GameManager.Tutorial_Monday);
-		
-		Application.LoadLevel ("Monday");
-	}
-	
-	public void GoToGame(){
-		//Time.timeScale = 1;
-		
-		GameManager.Tutorial_Monday = false;
-		PlayerPrefsX.SetBool ("TutorialMonday", GameManager.Tutorial_Monday);
-		
-		Application.LoadLevel ("Monday");
-	}
-	
-	
+
 }
