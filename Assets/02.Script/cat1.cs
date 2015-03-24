68532dd519e1f60d4a40ea3485b9a0dd667daa6d
sue0711 <godqlswlq@gmail.com>
2015-03-25 02:43:23 +0900

    고양양양

--- Assets/02.Script/cat1.cs
+++ Assets/02.Script/cat1.cs
@@ -10,18 +10,26 @@ public class cat1 : MonoBehaviour{
 
 	Animator catAnimator;
 
+	public new Transform myTransform;
+	public new Transform pTransform;
 
 	// Use this for initialization
 	void Start () {
+		myTransform = transform;
 		player = GameObject.Find ("player");
+		pTransform = player.transform;
 		catAnimator = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player.transform.position.x > player_pos) {
+		if (pTransform.position.x > player_pos) {
 			catAnimator.SetTrigger("start");
 			//this.GetComponent<catSprite1>().enabled = true;
+		}
+
+		if (pTransform.position.x > myTransform.position.x+5) {
+			player.GetComponent<CatchedbyCat1>().enabled = false;
 		}
 	}
 
--- Assets/02.Script/cat2.cs
+++ Assets/02.Script/cat2.cs
@@ -10,10 +10,15 @@ public class cat2 : MonoBehaviour{
 
 	Animator catAnimator;
 
+	public new Transform myTransform;
+	public new Transform pTransform;
+
 
 	// Use this for initialization
 	void Start () {
+		myTransform = transform;
 		player = GameObject.Find ("player");
+		pTransform = player.transform;
 		catAnimator = this.GetComponent<Animator> ();
 	}
 	
@@ -22,6 +27,10 @@ public class cat2 : MonoBehaviour{
 		if (player.transform.position.x > player_pos) {
 			catAnimator.SetTrigger("start");
 			//this.GetComponent<catSprite2>().enabled = true;
+		}
+
+		if (pTransform.position.x > myTransform.position.x+5) {
+			player.GetComponent<CatchedbyCat2>().enabled = false;
 		}
 	}
 
