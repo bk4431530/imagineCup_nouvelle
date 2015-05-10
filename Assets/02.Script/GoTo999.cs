ead3606b89ac5edaec3b9a16aaead0222c8ca5f1
sue0711 <godqlswlq@gmail.com>
2015-05-11 01:33:21 +0900

    배경 최적화

---
+++ Assets/02.Script/GoTo999.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoTo999 : MonoBehaviour {
+
+	private Vector3 startPosition;
+	private Vector3 position999;
+	private Vector3 playerPosition;
+
+	bool inCam;
+
+	Transform player;
+	// Use this for initialization
+	void Awake () {
+		startPosition = this.transform.position;
+		Debug.Log (this.gameObject.name + " : " + startPosition);
+		position999 = new Vector3 (999,999,999);
+		player = GameObject.Find ("player").transform;
+		inCam = false;
+	}
+
+	void Start(){
+		//this.transform.position = position999;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPosition = player.position;
+		if (startPosition.x > playerPosition.x-30 && startPosition.x < playerPosition.x+30 && !inCam) {
+			this.transform.position = startPosition;
+			inCam = true;
+			Debug.Log (this.gameObject.name + " in camera");
+		} else if((startPosition.x < playerPosition.x-30 || startPosition.x > playerPosition.x+30) && inCam) {
+			this.transform.position = position999;
+			Debug.Log (this.gameObject.name + " out camera");
+		}
+	}
+}
