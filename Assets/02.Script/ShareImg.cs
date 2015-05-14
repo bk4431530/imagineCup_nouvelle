de247c04cb1776735f58542618236f90d7fa8197
sue0711 <godqlswlq@gmail.com>
2015-05-14 10:47:52 +0900

    공유 하는듕...
    컴에서 캡쳐까진 됨..ㅠㅜ 프로젝트폴더에 ㅇㅇ

---
+++ Assets/02.Script/ShareImg.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.IO;
+
+public class ShareImg : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	private bool isProcessing = false;
+
+	public bool btnDown;
+
+	GameObject shareBtn;
+
+	void Start(){
+		shareBtn = GameObject.Find ("ShareBtn");
+	}
+	/*
+	void Update ()
+	{
+		if (Input.touches.Length > 0 && guiTexture.HitTest(Input.touches[0].position))
+		{
+			if(shareBtn.OnPointe)
+			{
+				shareBtn.image.enabled = false;
+			}
+			else if (Input.GetButtonUp("ShareBtn"))
+			{
+				if(!isProcessing)
+					StartCoroutine( ShareScreenshot() );
+			}
+		}
+		
+	}
+	*/
+
+	public void OnPointerDown (PointerEventData eventData){ 
+		Debug.Log ("OnPointerDown");
+		btnDown = true;
+		shareBtn.GetComponent<Image> ().enabled = false;
+	}
+	
+	public void OnPointerUp (PointerEventData eventData){
+
+		Debug.Log ("OnPointerUp");
+		btnDown = false;
+		if (!isProcessing)
+						//StartCoroutine (Capture());
+			StartCoroutine( ShareScreenshot() );
+	}
+
+	public IEnumerator Capture(){
+		// wait for graphics to render
+		yield return new WaitForEndOfFrame();
+		/*
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		// create the texture
+		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
+		
+		// put buffer into texture
+		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
+		
+		// apply
+		screenTexture.Apply();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		
+		byte[] dataToSave = screenTexture.EncodeToPNG();
+		*/
+		string destination = "screenshot.png";
+		Application.CaptureScreenshot(destination);
+		
+		//File.WriteAllBytes(destination, dataToSave);
+	}
+	
+	public IEnumerator ShareScreenshot()
+	{
+		isProcessing = true;
+
+		// wait for graphics to render
+		yield return new WaitForEndOfFrame();
+		/*
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		// create the texture
+		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
+		
+		// put buffer into texture
+		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
+		
+		// apply
+		screenTexture.Apply();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		
+		byte[] dataToSave = screenTexture.EncodeToPNG();
+
+		string destination = Path.Combine(Application.persistentDataPath,System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+
+		File.WriteAllBytes(destination, dataToSave);
+		*/
+
+		string filename = "screenshot.png";
+		Application.CaptureScreenshot(filename);
+
+		string path = System.IO.Path.Combine (Application.persistentDataPath,filename);
+
+		if(!Application.isEditor)
+		{
+			// block to open the file and share it ------------START
+			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + path);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "testo");
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
+			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+			
+			// option one WITHOUT chooser:
+			currentActivity.Call("startActivity", intentObject);
+			
+			// option two WITH chooser:
+			//AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "YO BRO! WANNA SHARE?");
+			//currentActivity.Call("startActivity", jChooser);
+			
+			// block to open the file and share it ------------END
+			
+		}
+		isProcessing = false;
+		shareBtn.GetComponent<Image>().enabled = true;
+	}
+}
